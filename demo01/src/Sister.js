ee765f93fb17e80d0c10db1d150f0553e4cc3ac8
GouYuQing <3319948815@qq.com>
2019-08-02 12:08:45 +0800

    add and delete

---
+++ demo01/src/Sister.js
@@ -0,0 +1,66 @@
+import React,{Component,Fragment} from 'react'
+//增加fragment不影响flex布局
+class Sister extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            inputValue:'',
+            list:['抹茶蛋糕','红豆冰沙']
+        }
+    }
+    render(){
+        return(
+            <Fragment>
+         
+                <div>
+                    {/* 数据绑定  事件绑定*/}
+                   <input  value={this.state.inputValue} onChange={this.inputChage.bind(this)}/>
+                   <button type="button" onClick={this.addList.bind(this)}>增加</button>
+                   </div>
+                   <ul>
+                       {/* <li>奶茶</li>
+                       <li>冰淇淋</li> */}
+                       {
+                           this.state.list.map((item,index)=>{
+                               return(
+                                <li key={index+item} onClick={this.deleteItem.bind(this,index)}>{item}</li>
+                               )
+                              
+                           })
+                       }
+                   </ul>
+              
+           
+            </Fragment>
+        )
+    }
+    inputChage(e){
+        // console.log(e.target.value);
+        // this.state.inputValue = e.target.value;无法改变
+        this.setState({
+            inputValue:e.target.value
+        })
+    }
+    addList(){
+        this.setState({
+            //扩展运算符
+            list:[...this.state.list,this.state.inputValue],
+            inputValue:''
+        })
+    }
+    deleteItem(index){
+        // console.log(index)
+        //不出错但是不允许会造成后期功能障碍
+        // this.state.list.splice(index,1)
+        // this.setState({
+        //     list:this.state.list
+        // })
+        let list = this.state.list
+        list.splice(index,1)
+        this.setState({
+            list:list
+        })
+
+    }
+}
+export default Sister
--- demo01/src/index.js
+++ demo01/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Sister from './Sister'
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(<Sister/>,document.getElementById('root'));
