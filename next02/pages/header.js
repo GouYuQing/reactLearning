293bab84dda20a5404bd58aecd4f076246836f00
GouYuQing <3319948815@qq.com>
2019-09-29 10:31:43 +0800

    next.js finished

--- next02/pages/header.js
+++ next02/pages/header.js
@@ -4,7 +4,7 @@ import Head from 'next/head'
 // import '../static/test.css'
 
 //antd 东西很多 所以我们需要按需加载
-import {Button} from 'antd' 
+// import {Button} from 'antd' 
 function Header(){
     return (
     <>
@@ -13,7 +13,7 @@ function Header(){
         <mate charSet='utf-8'></mate>
     </Head>
     hello header.js
-    <div><Button>尝试是否可以改变按钮样式</Button></div>
+    {/* <div><Button>尝试是否可以改变按钮样式</Button></div> */}
     </>)
 }
 export default Header;
