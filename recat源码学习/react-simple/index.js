da881d149f94bb9a24bf380b769f6bdcaaf0f7a9
GouYuQing <3319948815@qq.com>
2020-12-01 17:22:34 +0800

    react源码学习第一天

---
+++ recat源码学习/react-simple/index.js
@@ -0,0 +1,13 @@
+const ele = (
+  <div className="hello">
+    <h3 className="title">hello,world</h3>
+  </div>
+)
+/*
+createElement(tag,attrs,child1,child2)
+var ele = React.createElement("div", {
+    className: "hello"
+  }, React.createElement("h3", {
+  className: "title"
+}, "hello,world"));
+ */
--- recat源码学习/react-test/src/index.js
+++ recat源码学习/react-test/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// const ele = (
+//   <div className="hello">
+//     <h3 className="title">hello,world</h3>
+//   </div>
+// )
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+var ele = React.createElement("div", {
+    className: "hello"
+  }, React.createElement("h3", {
+  className: "title"
+}, "hello,world"));
+
+
+//jsx:javascript+xml 虚拟DOM 语法糖
+ReactDOM.render(ele,document.querySelector('#root'));
